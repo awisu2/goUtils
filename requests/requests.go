f9f7263de069e54e132ca2d5af6090ed0eccc559
awisu2 <awisu2@gmail.com>
2021-10-09 14:05:01 +0900

    宣言ミスを修正

--- requests/requests.go
+++ requests/requests.go
@@ -1,13 +1,16 @@
 package requests
 
-cosnt (
-	ACCEPT_ALL = "*"
+import "net/http"
+
+const (
+	ACCEPT_ALL   = "*"
 	ACCEPT_MEDIA = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
 
 	ACCEPT_LANGUAGE_JAPAN = "ja-JP,ja;q=0.9,en-US;q=0.8,en;q=0.7"
 
 	USERAGENT_CHROME = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36"
 )
+
 func Get(url string) (*http.Response, error) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
